dde0b62d89800497b5df5a1ae4d83cbd83c92f9b
codedbychavez <codedbychavez@gmail.com>
2023-04-02 19:09:53 -0300

    Cleaned up the starter-code

--- feature-integration.test.js
+++ feature-integration.test.js
@@ -1,22 +1,2 @@
-const axiosInstance = require('./utils/axios/axios-instance');
-const setupFeatureIntegration = require('./utils/feature/feature-integration');
-
-// Remove Test Resources
-const removeTestResources = async (productId) => {
-  return await axiosInstance
-    .delete(`products/${productId}`)
-    .then(() => console.log("Testing resources removed..."))
-    .catch((error) => {
-      throw new Error(error);
-    });
-};
-
-
-test('should return an object containing a property key', async () => { 
-  const data = await setupFeatureIntegration();
-  expect(data).toHaveProperty("key");
-  const { productId } = data;
-  await removeTestResources(productId);
- })
-
+// feature-integration.test.js
 
--- utils/axios/axios-instance.js
+++ utils/axios/axios-instance.js
@@ -1,14 +1,1 @@
-const axios = require('axios');
-
-const axiosInstance = axios.create({
-  baseURL: 'https://api.configcat.com/v1/',
-  headers: {
-    'X-CONFIGCAT-SDKKEY': 'YOUR-CONFIGCAT-SDKKEY'
-  },
-  auth: {
-    username: 'YOUR-AUTH-USERNAME',
-    password: 'YOUR-AUTH-PASSWORD'
-  }
-});
-
-module.exports = axiosInstance;
+// axios-instance.js
